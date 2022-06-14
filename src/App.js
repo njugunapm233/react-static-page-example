6228b17531d55945c8d57440069469d11c8dbb94
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2022-06-14 12:43:36 +0300

    why react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name=<h3>Peter</h3>
+console.log(name);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="bg-dark text-info">
+<h1>Static Page</h1>
+<ul>
+  <li>Pricing</li>
+  <li>About</li>
+  <li>Contact</li>
+</ul>
+  </nav>
   );
 }
 
+     
+  
+
 export default App;
+
+// NOTE: Why React
+// 1.) Its composable 
+// 2.) It Declarative
+//  3.) JSX - Declarative
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
 
+// ReactDOM.render("<h1>Hello React!</h1>", document.getElementById('root'))
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+  {/* <Navbar/> */}
     <App />
+    
   </React.StrictMode>
 );
 
